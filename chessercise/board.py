8c39694f07f43734285c0ad84bf87351e5e1e6a1
mmartin <mmartin@mezeo.com>
2016-04-25 09:44:37 -0500

    Correctly set current position on piece object

--- chessercise/board.py
+++ chessercise/board.py
@@ -28,16 +28,13 @@ class Board(object):
     def is_valid_position(self, pos):
         # sys.path.append('/opt/eclipse/plugins/org.python.pydev_4.5.5.201603221110/pysrc/')
         # import pydevd; pydevd.settrace()
-        ok_pos = True
-
         col = pos[0]
         row = pos[1]
         if (int(row) in range(1, 9)) and (ord(col) in range(ord('a'), ord('i'))):
-                ok_pos = True  # Position is OK
+            return True  # Position is OK
         else:
             print('Error: position %s is off the board' % pos)
-            ok_pos = False
-        return ok_pos
+            return False
 
     def is_valid_move(self, from_pos, to_pos):
         ok_move = True
@@ -54,7 +51,7 @@ class Board(object):
 
 
     def set_piece(self, piece, position):
-        from_pos = piece.get_position()
         self.board[position] = piece
+        piece.set_position(position)
 
         print self.board
