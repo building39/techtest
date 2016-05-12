61936f99f573cb4b069c17cbe2b088d601c10c46
mmartin <mmartin@mezeo.com>
2016-05-12 10:28:11 -0500

    Static data

---
+++ chessercise/statics.py
@@ -0,0 +1,11 @@
+COLUMNS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+
+PIECES = {
+    'bishop': 'B',
+    'knight': 'Kt',
+    'king': 'K',
+    'queen': 'Q',
+    'rook': 'R',
+    'pawn': 'P',
+}
+
